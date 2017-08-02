da35f3d39f55f56faef62187bd99b31804cc144d
afpatmin <patrick.minogue@appfoundry.se>
2017-08-02 17:04:12 +0200

    customer auth service compliance

--- lib/src/services/customer_auth_service.dart
+++ lib/src/services/customer_auth_service.dart
@@ -4,5 +4,5 @@ import 'package:vi_auth_client/vi_auth_client.dart';
 @Injectable()
 class CustomerAuthService extends ViAuthClient
 {
-  CustomerAuthService() : super("https://api.bokain.se/auth_customer/index.php/");
+  CustomerAuthService() : super("https://api.bokain.se/auth_customer/index.php/", "dahl_customers");
 }
