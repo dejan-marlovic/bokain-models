94714ac66ba8523782df11f2bbc362155e8a991d
afpatmin <patrick.minogue@appfoundry.se>
2017-10-21 13:37:25 +0200

    consultation work

--- lib/src/editable_model/consultation.dart
+++ lib/src/editable_model/consultation.dart
@@ -6,6 +6,12 @@ class Consultation extends EditableModel
   Consultation(String customer_id, [String id = null]) : super(id)
   {
     customerId = customer_id;
+    areaFace = false;
+    areaChest = false;
+    areaBack = false;
+    areaOther = false;
+
+    zones = new List();
   }
 
   @override
@@ -13,6 +19,12 @@ class Consultation extends EditableModel
   {
     customerId = d["customer_id"];
     userId = d["user_id"];
+    areaFace = d["area_face"];
+    areaChest = d["area_chest"];
+    areaBack = d["area_back"];
+    areaOther = d["area_other"];
+
+    zones = (d.containsKey("zones")) ? d["zones"] : new List();
   }
 
   @override
@@ -25,6 +37,17 @@ class Consultation extends EditableModel
 
   String get customerId => _data["customer_id"];
   String get userId => _data["user_id"];
+  bool get areaFace => _data["area_face"];
+  bool get areaChest => _data["area_chest"];
+  bool get areaBack => _data["area_back"];
+  bool get areaOther => _data["area_other"];
+  List<String> get zones => _data["zones"];
+
   void set customerId(String value) { _data["customer_id"] = value; }
   void set userId(String value) { _data["user_id"] = value; }
+  void set areaFace(bool value) { _data["area_face"] = value; }
+  void set areaChest(bool value) { _data["area_chest"] = value; }
+  void set areaBack(bool value) { _data["area_back"] = value; }
+  void set areaOther(bool value) { _data["area_other"] = value; }
+  void set zones(List<String> value) { _data["zones"] = value; }
 }
