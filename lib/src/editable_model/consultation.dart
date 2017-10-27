c6ab72a7fdca9549cfb90563de897cb8497669ae
afpatmin <patrick.minogue@appfoundry.se>
2017-10-27 15:58:16 +0200

    skintype symptomregistry

--- lib/src/editable_model/consultation.dart
+++ lib/src/editable_model/consultation.dart
@@ -10,6 +10,7 @@ class Consultation extends EditableModel
     areaChest = false;
     areaBack = false;
     areaOther = false;
+    rosacea = false;
     milia = false;
     pigmentation = false;
     closedComedonesLevel = 0;
@@ -18,7 +19,7 @@ class Consultation extends EditableModel
     pustulesLevel = 0;
     papulesLevel = 0;
     openComedonesLevel = 0;
-    rosaceaLevel = 0;
+    seborrheaLevel = 0;
     texture = "normal";
     skinTypeId = "7"; // normal
     productIds = new List();
@@ -37,6 +38,7 @@ class Consultation extends EditableModel
     areaChest = d["area_chest"];
     areaBack = d["area_back"];
     areaOther = d["area_other"];
+    rosacea = d["rosacea"];
     milia = d["milia"];
     pigmentation = d["pigmentation"];
     closedComedonesLevel = d["closed_comedones_level"];
@@ -45,7 +47,7 @@ class Consultation extends EditableModel
     pustulesLevel = d["pastules_level"];
     papulesLevel = d["papules_level"];
     openComedonesLevel = d["open_comedones_level"];
-    rosaceaLevel = d["rosacea_level"];
+    seborrheaLevel = d["seborrhea_level"];
     texture = d["texture"];
     skinTypeId = d["skin_type_id"];
     productIds = (d.containsKey("product_ids")) ? d["product_ids"] : new List();
@@ -58,11 +60,6 @@ class Consultation extends EditableModel
     Map<String, String> table = new Map();
     table["created"] = ModelBase.timestampFormat(created);
     return table;
-  }
-
-  void _evaluateSkinType()
-  {
-    skinTypeId = "9";
   }
 
   String get customerId => _data["customer_id"];
@@ -80,9 +77,12 @@ class Consultation extends EditableModel
   int get nodulesLevel => _data["nodules_level"];
   int get papulesLevel => _data["papules_level"];
   int get pustulesLevel =>_data["pustules_level"];
-  int get rosaceaLevel => _data["rosacea_level_level"];
+  int get seborrheaLevel => _data["seborrhea_level"];
+  bool get rosacea => _data["rosacea"];
   bool get milia => _data["milia"];
   bool get pigmentation => _data["pigmentation"];
+  bool get surfaceBloodVessels => _data["surface_blood_vessels"];
+  bool get acneScars => _data["acne_scars"];
   String get texture => _data["texture"];
   String get skinTypeId => _data["skin_type_id"];
   List<String> get productIds => _data["product_ids"];
@@ -97,16 +97,19 @@ class Consultation extends EditableModel
   void set areaChest(bool value) { _data["area_chest"] = value; }
   void set areaBack(bool value) { _data["area_back"] = value; }
   void set areaOther(bool value) { _data["area_other"] = value; }
-  void set openComedonesLevel(int value) { _data["open_comedones_level"] = value; _evaluateSkinType(); }
-  void set closedComedonesLevel(int value) { _data["closed_comedones_level"] = value; _evaluateSkinType(); }
-  void set cystsLevel(int value) { _data["cysts_level"] = value;  _evaluateSkinType(); }
-  void set nodulesLevel(int value) { _data["nodules_level"] = value; _evaluateSkinType(); }
-  void set papulesLevel(int value) { _data["papules_level"] = value; _evaluateSkinType(); }
-  void set pustulesLevel(int value) { _data["pustules_level"] = value; _evaluateSkinType(); }
-  void set rosaceaLevel(int value) { _data["rosacea_level_level"] = value; _evaluateSkinType(); }
-  void set milia(bool value) { _data["milia"] = value; _evaluateSkinType(); }
-  void set pigmentation(bool value) { _data["pigmentation"] = value; _evaluateSkinType(); }
-  void set texture(String value) { _data["texture"] = value; _evaluateSkinType(); } /* dry, normal, oily */
+  void set openComedonesLevel(int value) { _data["open_comedones_level"] = value; }
+  void set closedComedonesLevel(int value) { _data["closed_comedones_level"] = value; }
+  void set cystsLevel(int value) { _data["cysts_level"] = value; }
+  void set nodulesLevel(int value) { _data["nodules_level"] = value; }
+  void set papulesLevel(int value) { _data["papules_level"] = value; }
+  void set pustulesLevel(int value) { _data["pustules_level"] = value; }
+  void set seborrheaLevel(int value) { _data["seborrhea_level"] = value; }
+  void set rosacea(bool value) { _data["rosacea"] = value; }
+  void set milia(bool value) { _data["milia"] = value; }
+  void set pigmentation(bool value) { _data["pigmentation"] = value; }
+  void set surfaceBloodVessels(bool value) { _data["surface_blood_vessels"] = value; }
+  void set acneScars(bool value) { _data["acne_scars"] = value; }
+  void set texture(String value) { _data["texture"] = value; } /* dry, normal, oily */
   void set productIds(List<String> value)  { _data["product_ids"] = value; }
   void set zones(List<String> value) { _data["zones"] = value; }
   void set skinTypeId(String value) { _data["skin_type_id"] = value; }
--- lib/src/static_model/skin_type.dart
+++ lib/src/static_model/skin_type.dart
@@ -2,5 +2,10 @@ import 'package:fo_components/fo_components.dart';
 
 class SkinType extends FoModel
 {
-  SkinType(String id, String name) : super(id, name);
+  SkinType(String id, String name, this.symptomsRegistry) : super(id, name)
+  {
+    if (symptomsRegistry == null) symptomsRegistry = new List();
+  }
+
+  List<Map<String, dynamic>> symptomsRegistry;
 }
